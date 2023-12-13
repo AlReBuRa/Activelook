e07dfc920a01d8d9c4bd5876280d278b118eb32c
Morgan Guillou <guillou.morgan@gmail.com>
2023-12-13 10:57:37 +0100

    Add an open methode in UdpServer

--- android/demo-app/src/main/java/com/activelook/demo/UdpServer.java
+++ android/demo-app/src/main/java/com/activelook/demo/UdpServer.java
@@ -1,8 +1,6 @@
 package com.activelook.demo;
 
-import android.os.Handler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,73 +10,81 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.nio.charset.StandardCharsets;
 
+
 public class UdpServer implements Runnable {
 
-    // The count member variable is shared between multiple threads
-    // that are executing the same instance of the MyTask runnable.
-    private Object object;
-    private JSONObject ILSobject;
-    private String strData;
+    public int Port = 10562;
+    public String Host = "239.255.255.255";
+    public int Timeout = 5000;
+    private MulticastSocket socket = null;
 
-    private Object a;
-    private Object b;
 
     // The object member variable can be shared between multiple threads if the same object instance is passed to MyTask executing in multiple threads
-    public UdpServer(String strData) {
-        this.strData = strData;
-        ILSobject = MainActivity.ILSobject;
+    public UdpServer() {
+
     }
 
-    public UdpServer(Object object, JSONObject ilsData) {
-        //this.IlsData = ilsData;
-        this.object = object;
-        ILSobject = MainActivity.ILSobject;
+    public void open(String host, int port, int timeout){
+        this.Host = host;
+        this.Port = port;
+        this.Timeout = timeout;
+
+        if(socket != null){
+            socket.disconnect();
+            socket.close();
+        }
+        try {
+            InetAddress group = InetAddress.getByName(Host);
+            socket = null;
+            socket = new MulticastSocket(Port);
+            socket.joinGroup(group);
+            socket.setSoTimeout(Timeout);
+            System.out.println("Open UDP multicast socket in group " + Host + " on port: " + Port);
+        }
+        catch (Exception ex) {
+            System.out.println("Error to create socket on port: " + Port);
+        }
 
     }
 
     @Override
     public void run() {
 
-        String threadName = Thread.currentThread().getName();
-
         // this.object
-        System.out.printf("[%s] %s\n", threadName, this.object);
+        System.out.printf("Start thread to received variables in UDP multicast (" + Thread.currentThread().getName() + ")");
 
-        Handler hd;
-        MulticastSocket socket = null;
-        InetAddress group = null;
 
-        try {
-            socket = new MulticastSocket(10562);
-            group = InetAddress.getByName("239.255.255.255");
-            socket.joinGroup(group);
-            System.out.println("Create socket on port: " + 10562);
-        }
-        catch (Exception ex) {
-            System.out.println("Error to create socket on port: ");
-
-        }
-
-        boolean running = true;
         System.out.println("Start receiving");
         DatagramPacket packet;
-        byte[] buf = new byte[4096];
-        while (running) {
-            packet = new DatagramPacket(buf, buf.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                System.out.println(e);
-                throw new RuntimeException(e);
-            }
-            this.strData = new String(buf, StandardCharsets.UTF_8);
+        int bufferSize = 4096;
+        byte[] buf = new byte[bufferSize];
+        packet = new DatagramPacket(buf, buf.length);
+
+
+        while (true) {
 
             try {
-                MainActivity.ILSobject = new JSONObject(this.strData);
-                synchronized (ILSobject) {
-                    ILSobject = new JSONObject(this.strData);
+                if(socket != null)
+                    socket.receive(packet);
+                else {
+                    System.out.println("UDP multicast socket is null");
+                    Thread.sleep(200);
+                    continue;
                 }
-                //System.out.println("TopAwsMeas: " + ILSobject.getString("TopAwsMeas"));
+
+            } catch (IOException e) {
+                String errorMessage = "An error occurred: " + e.getMessage();
+                System.out.println(errorMessage);
+                continue;
+            } catch (InterruptedException e) {
+                System.out.println("Error in thread sleep !!!");
+                throw new RuntimeException(e);
+            }
+
+            try {
+                String strData = new String(buf, StandardCharsets.UTF_8);
+                MainActivity.IlsObject = new JSONObject(strData);
+                //System.out.println("TopAwsMeas: " + MainActivity.IlsObject.getString("TopAwsMeas"));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -86,6 +92,28 @@ public class UdpServer implements Runnable {
 
 
         }
-        socket.close();
+
     }
+
+    /*
+    public void postToastMessage(final String message) {
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.getContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private Toast toast(Object data) {
+        Log.d("viewerCommands", data.toString());
+        Toast toast = Toast.makeText(this, data.toString(), Toast.LENGTH_SHORT);
+        toast.show();
+        return toast;
+    }
+
+     */
 }
